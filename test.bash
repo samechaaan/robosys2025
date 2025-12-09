0255eadf36303759ef0d123e2b67ca9697b31ea4
Kanon Ode <s24c1028wf@s.chibakoudai.jp>
2025-12-09 15:45:34 +0900

    test.bash updated

--- test.bash
+++ test.bash
@@ -3,11 +3,14 @@
 # SPDX-License-Identifier: GPL-3.0-only
 
 ng () {
-      echo ${1}行目が違うよ
+      echo "${1}行目が違うよ"
       res=1
 }
 
 res=0
+
+out=$(echo 2026 | python3 gengo.py)
+[ "${out}" = "不明" ] || ng "$LINENO"
 
 out=$(echo 2025 | python3 gengo.py)
 [ "${out}" = "令和7年" ] || ng "$LINENO"
@@ -23,6 +26,9 @@ out=$(echo 1926 | python3 gengo.py)
 
 out=$(echo 1868 | python3 gengo.py)
 [ "${out}" = "明治元年" ] || ng "$LINENO"
+
+out=$(echo 1867 | python3 gengo.py)
+[ "${out}" = "明治以前" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit $res
